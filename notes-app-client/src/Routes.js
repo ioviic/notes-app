b9887afd3f94cea28668277b9804f451e8be45c6
ioviic <>
2019-02-27 21:37:24 +0200

    add Amplify and add Login page for login

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
